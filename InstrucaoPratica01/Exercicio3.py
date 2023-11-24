ae2d4c443b5e35fb0bac1b973c7727c0e49bf64e
Biancardy <biancard.lima@gmail.com>
2023-11-24 11:49:36 -0300

    Exercicio3-parte3

--- InstrucaoPratica01/Exercicio3.py
+++ InstrucaoPratica01/Exercicio3.py
@@ -1,7 +1,10 @@
-for i in range(10):
-    caractere = chr(ord('0') + i)
-    codigo_numerico = ord(caractere)
+caractere_usuario = input("Digite um caractere: ")
+
+if len(caractere_usuario) == 1:
+    codigo_numerico = ord(caractere_usuario)
     codigo_octal = oct(codigo_numerico)
     codigo_hexadecimal = hex(codigo_numerico)
     
-    print(f"'{caractere}' - Decimal: {codigo_numerico}, Octal: {codigo_octal}, Hexadecimal: {codigo_hexadecimal}")
+    print(f"'{caractere_usuario}' - Decimal: {codigo_numerico}, Octal: {codigo_octal}, Hexadecimal: {codigo_hexadecimal}")
+else:
+    print("Por favor, digite exatamente um caractere.")
