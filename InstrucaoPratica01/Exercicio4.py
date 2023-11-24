cd6483395f5f2fde8ffb4d8ea122fc08b1ca675a
Biancardy <biancard.lima@gmail.com>
2023-11-24 12:08:24 -0300

    Exercicio4-parte 1

---
+++ InstrucaoPratica01/Exercicio4.py
@@ -0,0 +1,13 @@
+nome = "Biancardy Lima"
+
+primeiro_caractere = nome[0]
+print(f"Primeiro caractere: {primeiro_caractere}")
+
+primeiros_tres_caracteres = nome[:3]
+print(f"Primeiros três caracteres: {primeiros_tres_caracteres}")
+
+a_partir_da_posicao_5 = nome[5:]
+print(f"A partir da posição 5: {a_partir_da_posicao_5}")
+
+ultimos_tres_caracteres = nome[-3:]
+print(f"Últimos três caracteres: {ultimos_tres_caracteres}")
