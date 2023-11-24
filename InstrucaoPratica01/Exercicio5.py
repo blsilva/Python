b8911bee8371d3046a880cdbc59dea35efc362a5
Biancardy <biancard.lima@gmail.com>
2023-11-24 12:30:44 -0300

    Exercicio5-parte final

--- InstrucaoPratica01/Exercicio5.py
+++ InstrucaoPratica01/Exercicio5.py
@@ -1,5 +1,3 @@
-import sys
-
 a = 5.0
 b = 2.0
 
@@ -30,11 +28,30 @@ print(f"x depois de multiplicação: {x}")
 x /= y  
 print(f"x depois de divisão: {x}")
 
-x **= y 
+x **= y  
 print(f"x depois de exponenciação: {x}")
+
+# Maior e menor potência de 2 representável com float
+import sys
 
 maior_potencia_de_2 = sys.float_info.max_exp
 menor_potencia_de_2 = sys.float_info.min_exp
 
 print(f"\nMaior potência de 2 representável com float: 2^{maior_potencia_de_2}")
 print(f"Menor potência de 2 representável com float: 2^{menor_potencia_de_2}")
+
+# Demonstração da imutabilidade de variáveis numéricas
+num_inteiro = 5
+
+# Criando uma nova variável com o resultado da operação
+novo_num_inteiro = num_inteiro + 2
+
+print(f"\nValor original: {num_inteiro}")
+print(f"Novo valor após operação: {novo_num_inteiro}")
+
+# Verificação dos métodos disponíveis para variáveis de ponto flutuante
+num_ponto_flutuante = 3.14
+
+# Mostrando os métodos disponíveis para variáveis de ponto flutuante
+print("\nMétodos disponíveis para variáveis de ponto flutuante:")
+print(dir(num_ponto_flutuante))
