34fb3d818013abac22d92555289529a3a690144b
Biancardy <biancard.lima@gmail.com>
2023-11-24 11:57:18 -0300

    Exercicio3-Parte final

--- InstrucaoPratica01/Exercicio3.py
+++ InstrucaoPratica01/Exercicio3.py
@@ -8,3 +8,7 @@ if len(caractere_usuario) == 1:
     print(f"'{caractere_usuario}' - Decimal: {codigo_numerico}, Octal: {codigo_octal}, Hexadecimal: {codigo_hexadecimal}")
 else:
     print("Por favor, digite exatamente um caractere.")
+
+#Em Python, os caracteres especiais, como 'ç' e 'ã',são tratados como parte da codificação de 
+#caracteres utilizada.Python suporta Unicode, o que permite o uso de caracteres especiais e 
+#a manipulação de diferentes conjuntos de caracteres.
