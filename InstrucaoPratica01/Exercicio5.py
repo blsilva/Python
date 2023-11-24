98910ffe8e5599ac80ac496ec2fdc8f4da7c9465
Biancardy <biancard.lima@gmail.com>
2023-11-24 12:23:10 -0300

    Exercicio5-parte1

---
+++ InstrucaoPratica01/Exercicio5.py
@@ -0,0 +1,34 @@
+a = 5.0
+b = 2.0
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+exponenciacao = a ** b
+resto = a % b
+
+print(f"Soma: {soma}")
+print(f"Subtração: {subtracao}")
+print(f"Multiplicação: {multiplicacao}")
+print(f"Divisão: {divisao}")
+print(f"Exponenciação: {exponenciacao}")
+print(f"Resto da divisão: {resto}")
+
+x = 10.0
+y = 3.0
+
+x += y  
+print(f"x depois de adição: {x}")
+
+x -= y 
+print(f"x depois de subtração: {x}")
+
+x *= y  
+print(f"x depois de multiplicação: {x}")
+
+x /= y 
+print(f"x depois de divisão: {x}")
+
+x **= y  
+print(f"x depois de exponenciação: {x}")
