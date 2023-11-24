225127693aeff78cb9fd354cf66b06d303e9164b
Biancardy <biancard.lima@gmail.com>
2023-11-24 11:48:26 -0300

    Exercicio3-parte2

--- InstrucaoPratica01/Exercicio3.py
+++ InstrucaoPratica01/Exercicio3.py
@@ -1,4 +1,7 @@
 for i in range(10):
     caractere = chr(ord('0') + i)
     codigo_numerico = ord(caractere)
-    print(f"'{caractere}' - {codigo_numerico}")
+    codigo_octal = oct(codigo_numerico)
+    codigo_hexadecimal = hex(codigo_numerico)
+    
+    print(f"'{caractere}' - Decimal: {codigo_numerico}, Octal: {codigo_octal}, Hexadecimal: {codigo_hexadecimal}")
