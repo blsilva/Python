a86ca4963b89e657caa991df07721ac3d05fcae7
Biancardy <biancard.lima@gmail.com>
2023-11-24 12:27:31 -0300

    exercicio5-parte 2

--- InstrucaoPratica01/Exercicio5.py
+++ InstrucaoPratica01/Exercicio5.py
@@ -1,3 +1,5 @@
+import sys
+
 a = 5.0
 b = 2.0
 
@@ -6,29 +8,33 @@ subtracao = a - b
 multiplicacao = a * b
 divisao = a / b
 exponenciacao = a ** b
-resto = a % b
 
 print(f"Soma: {soma}")
 print(f"Subtração: {subtracao}")
 print(f"Multiplicação: {multiplicacao}")
 print(f"Divisão: {divisao}")
 print(f"Exponenciação: {exponenciacao}")
-print(f"Resto da divisão: {resto}")
 
 x = 10.0
 y = 3.0
 
-x += y  
+x += y 
 print(f"x depois de adição: {x}")
 
-x -= y 
+x -= y  
 print(f"x depois de subtração: {x}")
 
 x *= y  
 print(f"x depois de multiplicação: {x}")
 
-x /= y 
+x /= y  
 print(f"x depois de divisão: {x}")
 
-x **= y  
+x **= y 
 print(f"x depois de exponenciação: {x}")
+
+maior_potencia_de_2 = sys.float_info.max_exp
+menor_potencia_de_2 = sys.float_info.min_exp
+
+print(f"\nMaior potência de 2 representável com float: 2^{maior_potencia_de_2}")
+print(f"Menor potência de 2 representável com float: 2^{menor_potencia_de_2}")
